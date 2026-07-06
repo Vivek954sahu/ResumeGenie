4a2cbb26496f2ee0b8713083dc0510e0c17cec90
vivek954sahu <vivek954sahu@gmail.com>
2026-07-06 19:43:05 +0530

    Chore: Server setup

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+import { env, loadEnv } from "./src/config/env.js";
+import app from "./src/app.js";
+import http from "http";
+import { connectDB } from "./src/config/db.js";
+import { logger } from "./src/utils/logger.js";
+
+loadEnv();
+
+const server = http.createServer(app);
+
+const PORT = env.port || 3000;
+
+// Start Server
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        // DB connection success
+        server.listen(PORT, () => {
+            logger.info(`Server is running on port: ${PORT}`);
+        });
+    } catch (error) {
+        logger.error("Failed to start server!", {
+            error: error.message
+        });
+
+        process.exit(1);
+    }
+};
+
+startServer();
