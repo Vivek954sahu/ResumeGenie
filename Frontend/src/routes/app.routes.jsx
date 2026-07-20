637bcfb8f42da9aebfcaa3fa5b323befe295220d
vivek954sahu <vivek954sahu@gmail.com>
2026-07-20 17:00:51 +0530

    Update: Client App Router Updated.

--- Frontend/src/routes/app.routes.jsx
+++ Frontend/src/routes/app.routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import Login from "../features/auth/pages/Login.jsx";
 import Register from "../features/auth/pages/Register.jsx";
+import ProtectedRoute from "../features/auth/components/ProtectedRoute.jsx";
+import Home from "../features/interview/pages/Home.jsx";
+import Interview from "../features/interview/pages/Interview.jsx";
 
 
 export const router = createBrowserRouter([
@@ -13,4 +16,14 @@ export const router = createBrowserRouter([
         path: "/register",
         element: <Register />
     },
+
+    {
+        path: "/",
+        element: <ProtectedRoute><Home /></ProtectedRoute>
+    },
+
+    {
+        path: "/interview",
+        element: <ProtectedRoute><Interview /></ProtectedRoute>
+    }
 ]);
